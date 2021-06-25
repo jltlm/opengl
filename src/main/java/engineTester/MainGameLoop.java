d7980ed934baa62d94f5d40bbec72f8b33cac5da
jltlam <jlam011235@gmail.com>
2021-06-24 21:37:41 -0400

    finished tutorial 10 --> loaded model!

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -28,87 +29,11 @@ public class MainGameLoop {
         StaticShader shader = new StaticShader();
         Renderer renderer = new Renderer(shader);
 
-        float[] vertices = {
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
 
-                -0.5f,0.5f,0.5f,
-                -0.5f,-0.5f,0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                0.5f,0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,-0.5f,
-                -0.5f,-0.5f,-0.5f,
-                -0.5f,-0.5f,0.5f,
-                -0.5f,0.5f,0.5f,
-
-                -0.5f,0.5f,0.5f,
-                -0.5f,0.5f,-0.5f,
-                0.5f,0.5f,-0.5f,
-                0.5f,0.5f,0.5f,
-
-                -0.5f,-0.5f,0.5f,
-                -0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,-0.5f,
-                0.5f,-0.5f,0.5f
-
-        };
-
-        float[] textureCoords = {
-
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0,
-                0,0,
-                0,1,
-                1,1,
-                1,0
-
-
-        };
-
-        int[] indices = {
-                0,1,3,
-                3,1,2,
-                4,5,7,
-                7,5,6,
-                8,9,11,
-                11,9,10,
-                12,13,15,
-                15,13,14,
-                16,17,19,
-                19,17,18,
-                20,21,23,
-                23,21,22
-
-        };
-
-        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("kirishima_low_res"));
+        //RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        RawModel model = OBJLoader.loadObjModel("Stall Model and Texture/stall", loader);
+//        ModelTexture texture = new ModelTexture(loader.loadTexture("kirishima_low_res"));
+        ModelTexture texture = new ModelTexture(loader.loadTexture("Stall Model and Texture/stallTexture"));
         TexturedModel staticModel = new TexturedModel(model, texture);
 
         Entity entity = new Entity(staticModel, new Vector3f(0, 0, -1), 0, 0, 0, 1);
@@ -117,7 +42,7 @@ public class MainGameLoop {
 
         while (!Display.isCloseRequested()) {
 //            entity.increasePosition(0, 0, 0);
-            entity.increaseRotation(1, 1, 0);
+            entity.increaseRotation(0, 1, 0);
             camera.move();
             //game logic
             renderer.prepare();
